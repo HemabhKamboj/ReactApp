be86db299604206047ea526103a1db334f9e07b5
Hemabh Kamboj <hkamboj2603199@gmail.com>
2019-04-05 12:58:49 +0530

    class based components and function components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Component1 from './functional/component1'
+import Container1 from './container/container'
 
 class App extends Component {
  
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
       React
+      <Container1 nickname = "Hannu" />
       <Component1  name= "Hemabh" age= { 20}/>
       </div>
 
