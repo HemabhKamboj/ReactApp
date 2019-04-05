4ba61183f56de88268013c08800730ab60acd4cf
Hemabh Kamboj <hkamboj2603199@gmail.com>
2019-04-05 13:24:05 +0530

    working with multiple state properties

--- src/container/container.js
+++ src/container/container.js
@@ -6,21 +6,33 @@ class Container1 extends Component {
     {
         super(props)
         this.state = {
-            stateprop1 : "Our Initial State"
+            stateprop1 : "Our Initial State",
+            stateprop2 : 5
 
         }
     }
 
         changeState = () => (
-            this.setState({ stateprop1: "New State"})
+            this.setState({ stateprop2: this.state.stateprop2 + 1,
+                            stateprop1: this.state.stateprop1 + "H"})
+        )
+
+        changeState2 = () => (
+            this.setState({ stateprop1: this.state.stateprop1 + "H"})
         )
    render () {
     return (
         <div>
             <button onClick = {() => this.changeState()}> Change State </button>
+            <button onClick = {() => this.changeState2()}> Change State </button>
+            <br />
+
+            {this.state.stateprop2}
+            <br />
             {this.state.stateprop1}
         </div>
     )}
+
 
 }
 
